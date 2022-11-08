d684494ff0f351afd4ceb7ab92c2bac84696731e
colinmcnabb <mcnabb.colin.lee@gmail.com>
2022-11-08 11:24:19 -0700

    generate seeded random tuning curves for each unit

--- include/tuning/tuningcurves.h
+++ include/tuning/tuningcurves.h
@@ -1,9 +1,15 @@
 #ifndef TUNING_TUNINGCURVES_H
 #define TUNING_TUNINGCURVES_H
 
-#include <string>
 #include <vector>
 
 std::vector<std::vector<float>> ReadBaseCurves(const char* filename);
+
+std::vector<std::vector<float>> CreateTuningCurves(
+  std::vector<std::vector<float>> base_curves,
+  int seed,
+  int n_units);
+
+void MultiplyVectorByScalar(std::vector<float> v, float k);
 
 #endif
--- src/tuning/tuningcurves.cc
+++ src/tuning/tuningcurves.cc
@@ -1,7 +1,9 @@
 #include "tuning/tuningcurves.h"
 
+#include <algorithm>
 #include <iostream>
 #include <fstream>
+#include <random>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -10,7 +12,6 @@ std::vector<std::vector<float>> ReadBaseCurves(const char* filename) {
   std::string line;
   std::vector<std::vector<float>> base_curves;
   std::ifstream datafile (filename);
-  // std::cout << filename << std::endl;
 
   if (!datafile.is_open()) {     /* validate file open for reading */
         perror (("error while opening file " + std::string(filename)).c_str());
@@ -24,11 +25,40 @@ std::vector<std::vector<float>> ReadBaseCurves(const char* filename) {
     while (std::getline(s, val, ',')) {
       row.push_back(std::stof(val));
     }
-    std::cout << row[0] << std::endl;
+
     base_curves.push_back(row);
   }
 
   datafile.close();
-  // std::cout << "parsing complete" << std::endl;
+  base_curves.erase(base_curves.begin());  // remove indexing row (values 0-359)
+  
   return base_curves;
+}
+
+std::vector<std::vector<float>> CreateTuningCurves(
+    std::vector<std::vector<float>> base_curves,
+    int seed,
+    int n_units) {
+  std::vector<std::vector<float>> tuning_curves;
+  std::default_random_engine generator;
+  generator.seed(seed);
+  std::uniform_int_distribution<int> int_distribution(0, base_curves.size());
+  std::uniform_real_distribution<float> real_distribution(0.8, 1.2);
+  
+  for (int i=0; i < n_units; i++) {
+    int index = int_distribution(generator);
+    float scalar = real_distribution(generator);
+    std::vector<float> temp = base_curves[index];
+    int shift_val = (2*index)*(360/n_units);
+    std::rotate(temp.begin(), temp.begin()+shift_val, temp.end());
+    MultiplyVectorByScalar(temp, scalar);
+    tuning_curves.push_back(temp);
+  }
+  std::cout << tuning_curves.size() << std::endl;
+  return tuning_curves; 
+}
+
+void MultiplyVectorByScalar(std::vector<float> vector, float scalar){
+    std::transform(vector.begin(), vector.end(), vector.begin(), 
+                      [scalar](float v){ return v*scalar;});
 }
