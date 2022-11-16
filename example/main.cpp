774d4585d943de5fbefaa025ffe8fdff17e67e53
Colin McNabb <mcnabb.colin.lee@gmail.com>
2022-11-16 14:50:04 -0700

    increase operations in example

--- example/main.cpp
+++ example/main.cpp
@@ -1,10 +1,31 @@
+#include <cerrno>
 #include <iostream>
+#include <string>
 
 #include "spikes/firing.h"
 #include "spikes/tuning.h"
 
 int main() {
     std::cout << "Hello, world!" << std::endl;
-    return 0;
+    std::vector<std::vector<float>> base = ReadBaseCurves(
+        "../../data/basecurves.csv");
+    for (auto row_obj : base) {
+        for (auto elem : row_obj) {
+          std::cout << elem << ", ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
 
+    std::vector<std::vector<float>> tuningcurves =
+        CreateTuningCurves(base, 10050, 192);
+    
+    for (auto row_obj : tuningcurves) {
+        for (auto elem : row_obj) {
+          std::cout << elem << ", ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    return 0;
 }
