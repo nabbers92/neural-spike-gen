e71b40b397b8248c5a32b6a1f0bcac4c746b34ab
Colin McNabb <mcnabb.colin.lee@gmail.com>
2022-11-30 14:15:36 -0700

    update header definitions

--- include/spikes/firing.h
+++ include/spikes/firing.h
@@ -4,6 +4,9 @@
 
 int GetAngle(float vx, float vz);
 
+std::vector<int> ExtractColumn(const std::vector<std::vector<float>> &matrix,
+                               int col_idx);
+
 std::vector<int> GenerateSpikes(std::vector<float> velocity, float dt,
                                 int n_units,
                                 std::vector<std::vector<float>> tuning_curves,
