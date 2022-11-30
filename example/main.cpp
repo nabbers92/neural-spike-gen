06d7e6f7b0a985ba874d27a4cf5ca1bbb2c5f900
Colin McNabb <mcnabb.colin.lee@gmail.com>
2022-11-30 14:15:45 -0700

    add functionality to example

--- example/main.cpp
+++ example/main.cpp
@@ -6,26 +6,35 @@
 #include "spikes/tuning.h"
 
 int main() {
-  std::cout << "Hello, world!" << std::endl;
+  std::vector<float> velocity = {0.5, 0.5};
+  float dt = 0.008;
+  int n_units = 192;
+  int seed = 10050;
+
+  // std::cout << "Hello, world!" << std::endl;
   std::vector<std::vector<float>> base =
       ReadBaseCurves("../../data/basecurves.csv");
-  for (auto row_obj : base) {
-    for (auto elem : row_obj) {
-      std::cout << elem << ", ";
-    }
-    std::cout << std::endl;
-  }
-  std::cout << std::endl;
+  // for (auto row_obj : base) {
+  //   for (auto elem : row_obj) {
+  //     std::cout << elem << ", ";
+  //   }
+  //   std::cout << std::endl;
+  // }
+  // std::cout << std::endl;
 
   std::vector<std::vector<float>> tuningcurves =
-      CreateTuningCurves(base, 10050, 192);
+      CreateTuningCurves(base, seed, n_units);
 
-  for (auto row_obj : tuningcurves) {
-    for (auto elem : row_obj) {
-      std::cout << elem << ", ";
-    }
-    std::cout << std::endl;
-  }
-  std::cout << std::endl;
+  // for (auto row_obj : tuningcurves) {
+  //   for (auto elem : row_obj) {
+  //     std::cout << elem << ", ";
+  //   }
+  //   std::cout << std::endl;
+  // }
+  // std::cout << std::endl;
+
+  std::vector<int> spikesarray =
+      GenerateSpikes(velocity, dt, n_units, tuningcurves, seed);
+
   return 0;
 }
